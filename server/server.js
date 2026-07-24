c1e09bd691a6ebaee5619c283474bf6f6bb5cefa
Gopinath K <gopinathgopik2004@gmail.com>
2026-07-24 18:09:02 +0530

    Simplify CORS origin configuration

--- server/server.js
+++ server/server.js
@@ -44,10 +44,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cors({
-    origin: [
-  "http://localhost:5173",
-  "https://chat-application-lime-pi.vercel.app",
-];
+    origin:"https://chat-application-lime-pi.vercel.app",
     credentials: true
 }));
 
