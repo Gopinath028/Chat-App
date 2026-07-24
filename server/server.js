6ff4cf2492d3682b84ff55e833c5781e05fa6801
Gopinath K <gopinathgopik2004@gmail.com>
2026-07-24 18:07:00 +0530

    Allow CORS from additional origin

--- server/server.js
+++ server/server.js
@@ -44,7 +44,10 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+  "http://localhost:5173",
+  "https://chat-application-lime-pi.vercel.app",
+];
     credentials: true
 }));
 
